f4fac09ad4aba7386ece75d434d4a81bb211c394
Kevin Rauwolf <sweetpea-git@tentacle.net>
2018-01-14 13:01:07 -0800

    Improve fill bit handling when it crosses byte boundaries

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -1,6 +1,7 @@
 use nom::{anychar, digit, IResult, hex_u32};
 use errors::*;
 use super::sentence::AisSentence;
+use std::cmp;
 
 pub type BitCount = usize;
 
@@ -27,8 +28,8 @@ impl<'a> AisMessage<'a> {
 }
 
 fn unarmor<'a>(data: &'a [u8], fill_bits: BitCount) -> Result<Vec<u8>> {
-    let byte_count = (data.len() * 6) / 8 + ((data.len() * 6) % 8 != 0) as usize;
-    println!("Byte count is {}", byte_count);
+    let bit_count = data.len() * 6;
+    let byte_count = (bit_count / 8) + ((bit_count % 8 != 0) as usize);
     let mut output = vec![0; byte_count];
     let mut offset = 0;
     for byte in data {
@@ -41,13 +42,30 @@ fn unarmor<'a>(data: &'a [u8], fill_bits: BitCount) -> Result<Vec<u8>> {
         let offset_bit = offset % 8;
         output[offset_byte] |= unarmored >> offset_bit;
         if offset_bit > 2 {
+            // Continue into the next byte
             output[offset_byte + 1] |= unarmored << (8 - offset_bit);
         }
         offset += 6;
     }
     if fill_bits != 0 {
-        if let Some(final_byte) = output.last_mut() {
-            *final_byte &= 0b11111111u8 << fill_bits;
+        let bits_in_final_byte = match bit_count % 8 {
+            0 => 8,
+            1...7 => bit_count % 8,
+            _ => unreachable!(),
+        };
+        let final_idx = output.len() - 1;
+        {
+            let byte = &mut output[final_idx];
+            let shift = (8 - bits_in_final_byte) + cmp::min(fill_bits, bits_in_final_byte);
+            *byte &= match shift {
+                0...7 => 0xffu8 << shift,
+                8 => 0x0u8,
+                _ => unreachable!(),
+            };
+        }
+        if fill_bits > bits_in_final_byte {
+            let byte = &mut output[final_idx - 1];
+            *byte &= 0xffu8 << (fill_bits - bits_in_final_byte);
         }
     }
     Ok(output)
@@ -65,16 +83,37 @@ mod tests {
     }
 
     #[test]
-    fn unarmor_multi_bytes_aligned() {
+    fn unarmor_single_byte_fill() {
+        let input = b"9";
+        let result = unarmor(input, 4).unwrap();
+        assert_eq!([0b00000000,], &result[..]);
+    }
+
+    #[test]
+    fn unarmor_multi_bytes_unaligned() {
         let input = b"9q";
         let result = unarmor(input, 0).unwrap();
         assert_eq!([0b00100111, 0b10010000,], &result[..]);
     }
 
     #[test]
-    fn unarmor_multi_bytes_fill_bits() {
+    fn unarmor_multi_bytes_aligned() {
         let input = b"9qKr";
+        let result = unarmor(input, 0).unwrap();
+        assert_eq!([0b00100111, 0b10010110, 0b011111010], &result[..]);
+    }
+
+    #[test]
+    fn unarmor_multi_bytes_aligned_fill() {
+        let input = b"9qWr";
         let result = unarmor(input, 4).unwrap();
-        assert_eq!([0b00100111, 0b10010110, 0b011110000], &result[..]);
+        assert_eq!([0b00100111, 0b10011001, 0b11110000], &result[..]);
+    }
+
+    #[test]
+    fn unarmor_multi_bytes_unaligned_fill() {
+        let input = b"9qW";
+        let result = unarmor(input, 3).unwrap();
+        assert_eq!([0b00100111, 0b10011000, 0b00000000], &result[..]);
     }
 }

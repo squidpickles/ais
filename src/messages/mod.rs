152e6eadb9cee2f0d67eb70231f159e8ddfd34ac
Kevin Rauwolf <sweetpea-git@tentacle.net>
2023-02-12 22:22:31 -0800

    Disambiguate ShipType, make types visible

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -19,7 +19,7 @@ mod radio_status;
 pub mod standard_class_b_position_report;
 pub mod static_and_voyage_related_data;
 pub mod static_data_report;
-mod types;
+pub mod types;
 pub mod utc_date_response;
 
 pub use parsers::message_type;
